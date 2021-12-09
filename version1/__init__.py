a4b06ef58ab7bd7a53d13e9b92924b29c3a8d8f6
annashch <annashch@users.noreply.github.com>
2021-12-09 23:47:24 +0200

    Add files via upload

--- version1/__init__.py
+++ version1/__init__.py
@@ -18,38 +18,52 @@ class Constants(BaseConstants):
     num_rounds = 1
     punishment_cost = 5
     punish_size = 20
-    base_endowment=100
+    citizen_role = 'Citizen'
+    official_role = 'Official'
 
 class Subsession(BaseSubsession):
-# group and types are defined in the Subsession class
-    def creating_session(self):
-    # to set initial values in the subsession
-        self.group_randomly(fixed_id_in_group = True)
-        # built-in function that  shuffles players randomly but keeps the id constant
-        for g in self.get_groups():
-        # returns a list of all the groups in the subsession
-        # loop through the groups in the subsession
-            for p in g.get_players():
-            # get_players() -> returns a list of all the players in the subsession.
-            # loop through the players in the subsession (p is a player)
-                if p.id_in_group % 2 == 0:
-                  # id_in_group -> player's attribute (unique identifier)
-                  # if the id is even (via modulo operator)
-                  p.participant.vars['type']= 'OFFICIAL'
-                    # the participant is assigned to type "OFFICIAL"
-                else:
-                # if the participant id is odd
-                    p.participant.vars['type'] = 'CITIZEN'
-                    # the participant is assigned to type "CITIZEN"
+
+    def creating_session(subsession: Subsession):
+        players = subsession.get_players()
+
+for p in players:
+    p.is_official =  Constants.official_role
+    p.is_citizen= Constants.citizen_role
+
+if subsession.round_number == 1:
+        for player in subsession.get_players():
+            participant = player.participant
+            participant.endowment = random.choice([50, 100, 150])
+
+import itertools
+treatments = itertools.cycle(
+    itertools.product([50, 100, 150])
+    for p in subsession.get_players():
+    if p.is_citizen:
+        p.endowment = next(treatments)
+
+
 class Group(BaseGroup):
     pass
 
+class Official(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.role == Constants.official_role
+
+class Citizen(Page):
+
+    @staticmethod
+    def is_displayed(player):
+        return player.role == Constants.citizen_role
+
+class RoleAssignment(WaitPage):
+    after_all_players_arrive = 'set_payoffs'
+    title_text = 'Waiting Room'
+    body_text = 'Please wait! The experiment will continue momentarily'
+
 
 class Player(BasePlayer):
-    def role(self):
-        return self.participant.vars['type']
-    type = models.StringField()
-
     choice = models.BooleanField(
         labels=" Please choose if you want to demand a bribe or not ",
         choices=[
@@ -57,7 +71,8 @@ class Player(BasePlayer):
             [False, "Pay the amount they earned"]
         ]
     )
-    amount_demanded = models.CurrencyField()
+    amount_demanded = models.CurrencyField(label='Please indicate the amount of bribe demanded', max=20, min=0)
+
     offer_accepted = models.BooleanField(
         labels=" Please if you are willing to accept the bribe requested",
         choices=[
@@ -75,11 +90,14 @@ class Player(BasePlayer):
 
 
 # FUNCTIONS
-def set_payoffs(self):
-    p1 = self.get_player_by_id(1)
-    p2 = self.get_player_by_id(2)
-    p1.payoff = Constants.base_endowment + self.amount_demanded - Constants.punish_size
-    p2.payoff = Constants.base_endowment - amount_demanded - Constants.punishment_cost
+def set_payoffs(group:Group):
+    official = group.get_player_by_role(Constants.official_role)
+    citizen = group.get_player_by_role(Constants.citizen_role)
+
+for player.role == Constants.agent_role
+    official.payoff = participant.endowment + self.amount_demanded - Constants.punish_size
+
+    citizen.payoff = participant.endowment - amount_demanded - Constants.punishment_cost
 
 for amount_demanded in range (0,20)
     punish_prob = amount_demanded*0.04
@@ -92,51 +110,52 @@ class DECISION(Page):
     form_model = "player"
     form_fields = ["choice"]
     def is_displayed(player: Player):
-        return player.id_in_group == 2
+        return player.role == Constants.official_role
 
 class WaitForExtortion(WaitPage) :
     def is_displayed(player: Player) :
-        return player.id_in_group == 1
+        return player.role == Constants.citizen_role
 
 class AMOUNT(Page):
     form_model = "player"
     form_fields = ["amount_demanded"]
     def is_displayed(player: Player):
-        return player.id_in_group == 2
+        return player.role == Constants.official_role
 
 class WaitForAmount(WaitPage) :
     def is_displayed(player: Player) :
-        return player.id_in_group == 1
+        return  player.role == Constants.citizen_role
 
 class RESPONSE(Page):
     form_model = "player"
     form_fields = ["offer_accepted"]
     def is_displayed(player: Player):
-        return player.id_in_group == 1
+        return Constants.citizen_role
 
 class WaitForEngagement(WaitPage) :
     def is_displayed(player: Player) :
-        return player.id_in_group == 2
+        return Constants.official_role
 
 class Punishment(Page):
     form_model = "player"
     form_fields = ["punish_choice"]
     def is_displayed(player: Player):
-        return player.id_in_group == 1
+        return Constants.citizen_role
 
 class WaitForPunishment(WaitPage) :
     def is_displayed(player: Player) :
-        return player.id_in_group == 2
+        return Constants.official_role
 
 class ResultsWaitPage(WaitPage):
     @staticmethod
-    after_all_players_arrive = set_payoffs
+    after_all_players_arrive(group:Group):
+    if
 
 
 
 
 
-page_sequence = [MyPage, DECISION, WaitForExtortion,
+page_sequence = [Official,Citizen, DECISION, WaitForExtortion,
                  AMOUNT, WaitForAmount, RESPONSE,
                  WaitForEngagement, Punishment, WaitForPunishment,
-                 ResultsWaitPage, Results]
+                 ResultsWaitPage, ResultsWaitPage]
